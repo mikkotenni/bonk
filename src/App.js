800d6bfcd464bc7fdc38802adaf50c17669e8a37
mikkotenni <mikko.tenni@hotmail.com>
2024-03-07 19:17:48 +0200

    feat: setup basic router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const Header = styled.header`
   padding: 12px;
-`
+`;
+const Navigation = styled.nav`
+  display: flex;
+  gap: 12px;
+`;
+
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
 
 function App() {
   return (
-    <Header data-testid="app-header">
-      Bonk
-    </Header>
+    <>
+      <Header data-testid="app-header">Bonk</Header>
+      <Router>
+        <Navigation>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </Navigation>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
