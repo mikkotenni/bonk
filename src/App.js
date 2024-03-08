51821019b7051f416eaae3260d99e2a364f14707
mikkotenni <mikko.tenni@hotmail.com>
2024-03-08 19:13:08 +0200

    feat: use react-query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRoutes from "./AppRoutes";
 
 const Header = styled.header`
@@ -10,6 +11,8 @@ const Navigation = styled.nav`
   gap: 12px;
 `;
 
+const queryClient = new QueryClient();
+
 /**
  * App component
  * @description The main application component with header and main navigation.
@@ -17,16 +20,16 @@ const Navigation = styled.nav`
  */
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header data-testid="app-header">Bonk</Header>
       <Router>
         <Navigation>
           <Link to="/">Home</Link>
-          <Link to="/gadget">Gadget</Link>
+          <Link to="/gadgets">Gadgets</Link>
         </Navigation>
         <AppRoutes />
       </Router>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Gadget from "./components/Gadget";
+import Gadgets from "./components/Gadgets";
 
-const NotFound = () => <div>Whoopsie! 404 Not Found</div>;
+const NotFound = () => <p>Oopsy Daisy! 404 Not Found</p>;
 
 /**
  * AppRoutes component
- * @description Connects the routes with corresponding view components.
+ * @description Connects the routes and corresponding view components.
  * @returns {JSX.Element}
  */
 const AppRoutes = () => (
   <Routes>
-    <Route path="/gadget" element={<Gadget />} />
+    <Route path="/gadgets" element={<Gadgets />} />
     <Route path="/" element={<Home />} />
     <Route path="*" element={<NotFound />} />
   </Routes>
